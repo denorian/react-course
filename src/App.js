5bcc34dd7a813d8f1bafc8db77a4f8c0ad501a83
AlexanderBrovko <denorian@yandex.ru>
2020-03-10 17:51:53 +0300

    React JS course, lesson 29

--- src/App.js
+++ src/App.js
@@ -6,28 +6,7 @@ import Dialogs from "./components/Dialogs/Dialogs.jsx";
 import {BrowserRouter, Route} from "react-router-dom";
 import Profile from "./components/Profile/Profile";
 
-let dialogData = [
-    {id: 1, name: "Alex"},
-    {id: 2, name: "Valera"},
-    {id: 3, name: "Vlad"},
-    {id: 4, name: "Marat"},
-    {id: 5, name: "Ignat"}
-];
-
-let messsageData = [
-    {id: 1, message: "Hi!"},
-    {id: 2, message: "Hello"},
-    {id: 3, message: "How are you?"},
-    {id: 4, message: "Nice!"},
-    {id: 5, message: "And you?"},
-];
-let postData = [
-    {message: "Hi, how are you?", likesCount: 12},
-    {message: "It's my first post", likesCount: 11},
-    {message: "Init post", likesCount: 1}
-];
-
-const App = () => {
+const App = (props) => {
     return (
         // eslint-disable-next-line react/jsx-no-undef
         <BrowserRouter>
@@ -35,12 +14,9 @@ const App = () => {
                 <Header/>
                 <Navbar/>
                 <div className="app-wrapper-content">
-                    <Route path='/dialogs'>
-                        <Dialogs dialogData={dialogData} messsageData={messsageData} />
-                    </Route>
-                    <Route path='/profile'>
-                        <Profile postData={postData} />
-                    </Route>
+                    <Route path='/dialogs'
+                           render={() => <Dialogs dialogData={props.appState.dialogData} messsageData={props.appState.messsageData}/>}/>
+                    <Route path='/profile' render={() =>  <Profile postData={props.appState.post}/>}/>
                     <Route path='/news'>
                         this is news
                     </Route>
---
+++ src/state/state.js
@@ -0,0 +1,23 @@
+let State = {
+    post: [
+        {message: "Hi, how are you?", likesCount: 12},
+        {message: "It's my first post", likesCount: 11},
+        {message: "Init post", likesCount: 1}
+    ],
+    dialogData: [
+        {id: 1, name: "Alex"},
+        {id: 2, name: "Valera"},
+        {id: 3, name: "Vlad"},
+        {id: 4, name: "Marat"},
+        {id: 5, name: "Ignat"}
+    ],
+    messsageData : [
+        {id: 1, message: "Hi!"},
+        {id: 2, message: "Hello"},
+        {id: 3, message: "How are you?"},
+        {id: 4, message: "Nice!"},
+        {id: 5, message: "And you?"},
+    ]
+};
+
+export default State;
