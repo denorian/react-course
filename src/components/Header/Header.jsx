b9eaf01007c331af9af9637e9e6dbe112d6b5a19
AlexanderBrovko <denorian@yandex.ru>
2020-04-29 09:21:41 +0300

    React JS course, lesson 61 added authMe

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import s from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
             <img src='https://avatars.mds.yandex.net/get-pdb/1607455/c4840727-e476-4fa1-a9b9-2dc17aa2850a/s1200?webp=false' alt='100'/>
+            <div className={s.loginBlock}>
+                {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+            </div>
         </header>
     );
 };
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import dialogReducer from "./dialogs-reducer";
 import profileReducer from "./profile-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./user-reducer";
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers({
     dialogsPage: dialogReducer,
     profilePage: profileReducer,
     sidebar: sidebarReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 });
 
 let store = createStore(reducers);
