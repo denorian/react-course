eb0bb716babda6181ffb21b684884b612c8ee1ec
AlexanderBrovko <denorian@yandex.ru>
2020-05-06 10:22:10 +0300

    React JS course, lesson 69 SetState

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css'
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus/ProfileStatus";
 
 const ProfileInfo = (props) => {
 
@@ -15,7 +16,7 @@ const ProfileInfo = (props) => {
             </div>
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large} alt={props.profile.name}/>
-                Ava + description
+                <ProfileStatus/>
             </div>
         </div>
     );
